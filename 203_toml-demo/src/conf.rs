8ab9ce7ea5543938784beee4c91d53a316049934
liwei <liweilijie@gmail.com>
2021-09-27 15:19:50 +0800

    update readme.

--- 203_toml-demo/src/conf.rs
+++ 203_toml-demo/src/conf.rs
@@ -1,20 +1,20 @@
 pub(crate) mod basic_conf;
-pub(crate) mod poem_conf;
 pub(crate) mod error;
+pub(crate) mod poem_conf;
 
 use super::*;
-use std::fs::{self,File};
+use std::borrow::Cow;
+use std::fs::{self, File};
 use std::io::Read;
-use std::path::{PathBuf,Path};
-
+use std::path::{Path, PathBuf};
 
 use toml;
 
+pub use self::basic_conf::BasicConfig;
 pub use self::error::ConfigError;
 pub use self::poem_conf::PoemConfig;
-pub use self::basic_conf::BasicConfig;
 
-use crate::environment::{Environment,Environment::*};
+use crate::environment::{Environment, Environment::*};
 use std::collections::HashMap;
 
 pub const CONFIG_FILENAME: &str = "conf/Poem.toml";
