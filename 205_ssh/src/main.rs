692b184dca34e66642e2c6ad7179f8c9eca6ba44
liwei <liweilijie@gmail.com>
2022-04-12 14:18:37 +0800

    add ssh.

---
+++ 205_ssh/src/main.rs
@@ -0,0 +1,24 @@
+use std::io::prelude::*;
+use ssh2::Session;
+use std::net::TcpStream;
+
+fn main() {
+    // Connect to the ssh server
+    let tcp = TcpStream::connect("222.213.23.75:22216").unwrap();
+    let mut sess = Session::new().unwrap();
+    // 设置超时时间
+    sess.set_timeout(20 * 1000);
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+
+    sess.userauth_password("cat", "fils&1466").unwrap();
+    println!("{}", sess.authenticated());
+
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls -l").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", s);
+    channel.wait_close().unwrap();
+    println!("{}", channel.exit_status().unwrap());
+}
